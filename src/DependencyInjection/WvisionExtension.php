d4d204eb28350d2159499449a64ec1b4d838abe4
Aaron Gerig <geoisborn@gmail.com>
2017-07-31 17:31:45 +0200

    Add license as header comment

--- src/DependencyInjection/WvisionExtension.php
+++ src/DependencyInjection/WvisionExtension.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * w-vision
+ *
+ * LICENSE
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that is distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2017 Woche-Pass AG (http://www.w-vision.ch)
+ */
 
 namespace WvisionBundle\DependencyInjection;
 
