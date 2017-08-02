f0b85b2024d1afa9e04e3799f0b198f69e8eab1f
Aaron Gerig <geoisborn@gmail.com>
2017-08-02 11:46:22 +0200

    Add config tree configurationf file

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * w-vision
+ *
+ * LICENSE
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that is distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2017 Woche-Pass AG (https://www.w-vision.ch)
+ */
+
+namespace WvisionBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('wvision');
+        $rootNode
+            ->children()
+                // TODO: Configure config tree
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
