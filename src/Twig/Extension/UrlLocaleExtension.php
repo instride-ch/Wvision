7dfecb8e70b318977e07f6b44cb42eae5d3adfb7
Aaron Gerig <geoisborn@gmail.com>
2017-08-24 12:05:48 +0200

    TwigExtension: UrlLocale formatter

---
+++ src/Twig/Extension/UrlLocaleExtension.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * w-vision
+ *
+ * LICENSE
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that is distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2017 Woche-Pass AG (https://www.w-vision.ch)
+ */
+
+namespace WvisionBundle\Twig\Extension;
+
+class UrlLocaleExtension extends \Twig_Extension
+{
+    /**
+     * Returns a list of filters to add to the existing list.
+     *
+     * @return array An array of filters
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter(
+                'urlLocale',
+                [$this, 'getUrlLocale'],
+                ['is_safe' => ['html']]
+            )
+        ];
+    }
+
+    /**
+     * Return a pretty locale for the url.
+     *
+     * @param $locale
+     *
+     * @return string The modified locale
+     */
+    public function getUrlLocale($locale)
+    {
+        if (strlen($locale) > 2 || strpos($locale, '_') !== false) {
+            return strtolower(str_replace('_', '-', $locale));
+        }
+
+        return $locale;
+    }
+}
