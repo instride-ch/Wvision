57bf33e0c336ffb6b1de8132940bf69f53e62384
Aaron Gerig <geoisborn@gmail.com>
2017-10-03 11:33:21 +0200

    Refactored page loader, fixes #27

--- src/Resources/install/assets/js/main.js
+++ src/Resources/install/assets/js/main.js
@@ -1,10 +1,7 @@
 // Imports
 import Icons from 'uikit/dist/js/uikit-icons';
 // import Icons from 'uikit/dist/js/uikit-icons-img';
-import PageLoader from './utils/page-loader';
+import './utils/page-loader';
 
 // Activate icons plugin
 UIkit.use(Icons);
-
-// Execute Modules
-PageLoader();
--- src/Resources/install/assets/js/utils/page-loader.js
+++ src/Resources/install/assets/js/utils/page-loader.js
@@ -1,6 +1,6 @@
-export default function PageLoader() {
+(function pageLoader() {
   $(window).on('load', () => {
     const loader = $('.page-loader');
     UIkit.util.animate(loader, 'uk-animation-fade', 400, false, true).then(() => loader.remove());
   });
-}
+}(jQuery, UIkit));
