26cd40337ddad7b9b7854f1c85182b9fa111a26b
Aaron Gerig <geoisborn@gmail.com>
2017-12-27 13:34:06 +0100

    Adjust copyright

--- src/Resources/install/assets/js/main.js
+++ src/Resources/install/assets/js/main.js
@@ -1,6 +1,6 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
-// import Icons from 'uikit/dist/js/uikit-icons-img';
+// import Icons from 'uikit/dist/js/uikit-icons-images';
 import './utils/page-loader';
 
 UIkit.use(Icons);
