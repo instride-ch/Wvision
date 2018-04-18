f90158460110a3817a79071906d526407f2ae920
Aaron Gerig <a.gerig@w-vision.ch>
2018-04-18 17:17:38 +0200

    Add default error page

--- src/Resources/install/assets/js/main.js
+++ src/Resources/install/assets/js/main.js
@@ -2,7 +2,11 @@ import './base/uikit';
 import fontLoader from './utils/font-loader';
 import pageLoader from './utils/page-loader';
 import './utils/static-assets';
+import glitchImage from './modules/glitch-image';
 
 // Utils
 fontLoader();
 pageLoader();
+
+// Modules
+glitchImage();
