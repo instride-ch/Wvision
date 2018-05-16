124c95bb42e09f3733ab4d8778dc185c027c187a
Aaron Gerig <a.gerig@w-vision.ch>
2018-05-16 14:57:01 +0200

    Fixed imports for both, CSS and JavaScript

--- src/Resources/install/assets/js/base/uikit.js
+++ src/Resources/install/assets/js/base/uikit.js
@@ -1,10 +1,12 @@
 import UIkit from 'uikit/dist/js/uikit-core';
 // import Countdown from 'uikit/dist/js/components/countdown';
-// import GridParallax from 'uikit/dist/js/components/grid-parallax';
+// import Filter from 'uikit/dist/js/components/filter';
 // import Lightbox from 'uikit/dist/js/components/lightbox';
+// import lightboxPanel from 'uikit/dist/js/components/lightbox-panel';
 // import Notification from 'uikit/dist/js/components/notification';
 // import Parallax from 'uikit/dist/js/components/parallax';
 // import Slider from 'uikit/dist/js/components/slider';
+// import SliderParallax from 'uikit/dist/js/components/slider-parallax';
 // import Slideshow from 'uikit/dist/js/components/slideshow';
 // import Sortable from 'uikit/dist/js/components/sortable';
 // import Tooltip from 'uikit/dist/js/components/tooltip';
@@ -12,16 +14,22 @@ import UIkit from 'uikit/dist/js/uikit-core';
 import Icons from 'uikit/dist/js/uikit-icons';
 // import Icons from 'uikit/dist/js/uikit-icons-custom';
 
-// UIkit.use(Countdown);
-// UIkit.use(GridParallax);
-// UIkit.use(Lightbox);
-// UIkit.use(Notification);
-// UIkit.use(Parallax);
-// UIkit.use(Slider);
-// UIkit.use(Slideshow);
-// UIkit.use(Sortable);
-// UIkit.use(Tooltip);
-// UIkit.use(Upload);
+// Components
+// UIkit.component('countdown', Countdown);
+// UIkit.component('filter', Filter);
+// UIkit.component('lightbox', Lightbox);
+// UIkit.component('lightboxPanel', lightboxPanel);
+// UIkit.component('notification', Notification);
+// UIkit.component('parallax', Parallax);
+// UIkit.component('slider', Slider);
+// UIkit.component('sliderParallax', SliderParallax);
+// UIkit.component('slideshow', Slideshow);
+// UIkit.component('slideshowParallax', SliderParallax);
+// UIkit.component('sortable', Sortable);
+// UIkit.component('tooltip', Tooltip);
+// UIkit.component('upload', Upload);
+
+// Icons
 UIkit.use(Icons);
 
 export default UIkit;
