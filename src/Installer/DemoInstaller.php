34624086e6a2cef02fd81b6dcc034027db3d8d7f
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-11-12 15:01:43 +0100

    fix version count installer issue

--- src/Installer/DemoInstaller.php
+++ src/Installer/DemoInstaller.php
@@ -58,7 +58,22 @@ final class DemoInstaller
                 DELETE FROM `documents_page` WHERE `id`=1 OR `id`=2 OR `id`=3;
                 INSERT INTO `documents_page` VALUES (1, NULL, \'@AppBundle\\Controller\\DefaultController\', \'default\', NULL, \'Startseite\', \'\', \'a:0:{}\', NULL, 0, \'\', 0);
                 INSERT INTO `documents_page` VALUES (2, NULL, \'@AppBundle\\Controller\\DefaultController\', \'imprint\', NULL, \'Impressum\', \'\', \'a:0:{}\', NULL, NULL, \'\', 0);
-                INSERT INTO `documents_page` VALUES (3, NULL, \'@AppBundle\\Controller\\ErrorController\', \'error\', NULL, \'Fehler\', \'\', \'a:0:{}\', NULL, NULL, \'\', 0);'
+                INSERT INTO `documents_page` VALUES (3, NULL, \'@AppBundle\\Controller\\ErrorController\', \'error\', NULL, \'Fehler\', \'\', \'a:0:{}\', NULL, NULL, \'\', 0);
+                
+                -- Assets
+                DELETE FROM `assets` WHERE `id`=1;
+                INSERT INTO `assets` VALUES (1, 0, \'folder\', \'\', \'/\', NULL, 1368522989, 1368522989, 1, 1, \'\', 0, 0);
+                INSERT INTO `assets` VALUES (2, 1, \'folder\', \'documents\', \'/demo\', NULL, 1505140576, 1505140576, 1, 1, \'a:0:{}\', 0, 0);
+                INSERT INTO `assets` VALUES (3, 1, \'folder\', \'images\', \'/demo\', NULL, 1505140572, 1505140572, 1, 1, \'a:0:{}\', 0, 0);
+                INSERT INTO `assets` VALUES (4, 2, \'document\', \'muster_excel.xlsx\', \'/demo/documents/\', \'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet\', 1505141170, 1505141660, 2, 2, \'a:0:{}\', 0, 0);
+                INSERT INTO `assets` VALUES (5, 2, \'document\', \'muster_pdf.pdf\', \'/demo/documents/\', \'application/pdf\', 1505141170, 1505141653, 1, 1, \'a:1:{s:19:\"document_page_count\";s:1:\"1\";}\', 0, 0);
+                INSERT INTO `assets` VALUES (6, 2, \'document\', \'muster_powerpoint.pptx\', \'/demo/documents/\', \'application/vnd.openxmlformats-officedocument.presentationml.presentation\', 1505141170, 1505141669, 2, 2, \'a:0:{}\', 0, 0);
+                INSERT INTO `assets` VALUES (7, 2, \'document\', \'muster_word.docx\', \'/demo/documents/\', \'application/vnd.openxmlformats-officedocument.wordprocessingml.document\', 1505141170, 1505141676, 2, 2, \'a:0:{}\', 0, 0);
+                INSERT INTO `assets` VALUES (8, 3, \'image\', \'wvision_baum.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0, 0);
+                INSERT INTO `assets` VALUES (9, 3, \'image\', \'wvision_newyork.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0, 0);
+                INSERT INTO `assets` VALUES (10, 3, \'image\', \'wvision_pusteblume.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0, 0);
+                INSERT INTO `assets` VALUES (11, 3, \'image\', \'wvision_windraeder.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0, 0);
+                INSERT INTO `assets` VALUES (12, 3, \'image\', \'wvision_winter.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0, 0);'
             );
         }
         else {
@@ -75,7 +90,21 @@ final class DemoInstaller
                 DELETE FROM `documents_page` WHERE `id`=1 OR `id`=2 OR `id`=3;
                 INSERT INTO `documents_page` VALUES (1, NULL, \'@AppBundle\\Controller\\DefaultController\', \'default\', NULL, \'Startseite\', \'\', \'a:0:{}\', NULL, 0, \'\', 0);
                 INSERT INTO `documents_page` VALUES (2, NULL, \'@AppBundle\\Controller\\DefaultController\', \'imprint\', NULL, \'Impressum\', \'\', \'a:0:{}\', NULL, NULL, \'\', 0);
-                INSERT INTO `documents_page` VALUES (3, NULL, \'@AppBundle\\Controller\\ErrorController\', \'error\', NULL, \'Fehler\', \'\', \'a:0:{}\', NULL, NULL, \'\', 0);'
+                INSERT INTO `documents_page` VALUES (3, NULL, \'@AppBundle\\Controller\\ErrorController\', \'error\', NULL, \'Fehler\', \'\', \'a:0:{}\', NULL, NULL, \'\', 0);
+                
+                DELETE FROM `assets` WHERE `id`=1;
+                INSERT INTO `assets` VALUES (1, 0, \'folder\', \'\', \'/\', NULL, 1368522989, 1368522989, 1, 1, \'\', 0);
+                INSERT INTO `assets` VALUES (2, 1, \'folder\', \'documents\', \'/demo\', NULL, 1505140576, 1505140576, 1, 1, \'a:0:{}\', 0);
+                INSERT INTO `assets` VALUES (3, 1, \'folder\', \'images\', \'/demo\', NULL, 1505140572, 1505140572, 1, 1, \'a:0:{}\', 0);
+                INSERT INTO `assets` VALUES (4, 2, \'document\', \'muster_excel.xlsx\', \'/demo/documents/\', \'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet\', 1505141170, 1505141660, 2, 2, \'a:0:{}\', 0);
+                INSERT INTO `assets` VALUES (5, 2, \'document\', \'muster_pdf.pdf\', \'/demo/documents/\', \'application/pdf\', 1505141170, 1505141653, 1, 1, \'a:1:{s:19:\"document_page_count\";s:1:\"1\";}\', 0);
+                INSERT INTO `assets` VALUES (6, 2, \'document\', \'muster_powerpoint.pptx\', \'/demo/documents/\', \'application/vnd.openxmlformats-officedocument.presentationml.presentation\', 1505141170, 1505141669, 2, 2, \'a:0:{}\', 0);
+                INSERT INTO `assets` VALUES (7, 2, \'document\', \'muster_word.docx\', \'/demo/documents/\', \'application/vnd.openxmlformats-officedocument.wordprocessingml.document\', 1505141170, 1505141676, 2, 2, \'a:0:{}\', 0);
+                INSERT INTO `assets` VALUES (8, 3, \'image\', \'wvision_baum.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0);
+                INSERT INTO `assets` VALUES (9, 3, \'image\', \'wvision_newyork.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0);
+                INSERT INTO `assets` VALUES (10, 3, \'image\', \'wvision_pusteblume.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0);
+                INSERT INTO `assets` VALUES (11, 3, \'image\', \'wvision_windraeder.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0);
+                INSERT INTO `assets` VALUES (12, 3, \'image\', \'wvision_winter.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0);'
             );
         }
 
@@ -83,25 +112,6 @@ final class DemoInstaller
             '
             SET NAMES utf8mb4;
 
-            -- Assets
-            DELETE FROM `assets` WHERE `id`=1;
-            INSERT INTO `assets` VALUES (1, 0, \'folder\', \'\', \'/\', NULL, 1368522989, 1368522989, 1, 1, \'\', 0);
-            INSERT INTO `assets` VALUES (2, 1, \'folder\', \'documents\', \'/demo\', NULL, 1505140576, 1505140576, 1, 1, \'a:0:{}\', 0);
-            INSERT INTO `assets` VALUES (3, 1, \'folder\', \'images\', \'/demo\', NULL, 1505140572, 1505140572, 1, 1, \'a:0:{}\', 0);
-            INSERT INTO `assets` VALUES (4, 2, \'document\', \'muster_excel.xlsx\', \'/demo/documents/\', \'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet\', 1505141170, 1505141660, 2, 2, \'a:0:{}\', 0);
-            INSERT INTO `assets` VALUES (5, 2, \'document\', \'muster_pdf.pdf\', \'/demo/documents/\', \'application/pdf\', 1505141170, 1505141653, 1, 1, \'a:1:{s:19:\"document_page_count\";s:1:\"1\";}\', 0);
-            INSERT INTO `assets` VALUES (6, 2, \'document\', \'muster_powerpoint.pptx\', \'/demo/documents/\', \'application/vnd.openxmlformats-officedocument.presentationml.presentation\', 1505141170, 1505141669, 2, 2, \'a:0:{}\', 0);
-            INSERT INTO `assets` VALUES (7, 2, \'document\', \'muster_word.docx\', \'/demo/documents/\', \'application/vnd.openxmlformats-officedocument.wordprocessingml.document\', 1505141170, 1505141676, 2, 2, \'a:0:{}\', 0);
-            INSERT INTO `assets` VALUES (8, 3, \'image\', \'wvision_baum.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0);
-            INSERT INTO `assets` VALUES (9, 3, \'image\', \'wvision_newyork.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0);
-            INSERT INTO `assets` VALUES (10, 3, \'image\', \'wvision_pusteblume.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0);
-            INSERT INTO `assets` VALUES (11, 3, \'image\', \'wvision_windraeder.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0);
-            INSERT INTO `assets` VALUES (12, 3, \'image\', \'wvision_winter.jpg\', \'/demo/images/\', \'image/jpeg\', 1505141522, 1505141522, 1, 1, \'a:3:{s:10:\"imageWidth\";i:2048;s:11:\"imageHeight\";i:1366;s:25:\"imageDimensionsCalculated\";b:1;}\', 0);
-            
-            -- Objects
-            DELETE FROM `objects` WHERE `o_id`=1;
-            INSERT INTO `objects` VALUES (1, 0, \'folder\', \'\', \'/\', 999999, 1, 1368522989, 1368522989, 1, 1, 0, \'\', NULL);
-            
             -- Properties
             DELETE FROM `properties` WHERE `cid`=1 OR `cid`=2 OR `cid`=3; 
             INSERT INTO `properties` VALUES (1, \'document\', \'/\', \'language\', \'text\', \'de\', 1);
